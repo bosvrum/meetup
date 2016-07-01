3ae5560694f01ec484b692d0c981ca944e737f58
Catalin Boruga <catalinboruga@yahoo.com>
2016-07-01 14:03:59 +0300

    Set the routes constraints for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: "home#index"
+
+  # Api definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/'  do
+  end
 end
